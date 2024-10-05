502a021170c3b3d17d45af7f205daf66285c1056
sharpenedblade <sharpenedblade@proton.me>
2024-10-05 16:32:52 -0700

    Add initial script to build silverblue ISOs

---
+++ build-iso.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e
+
+IMAGE="ghcr.io/t2linux/fedora-silverblue:unstable"
+
+mkdir output
+
+cp config.toml .tmp-config.toml
+sed -i "s/REPLACE_WITH_IMAGE_NAME/$IMAGE/g" .tmp-config.toml
+
+podman pull "$IMAGE"
+
+podman run --rm -it --privileged \
+  --platform linux/amd64 \
+  --security-opt label=type:unconfined_t \
+  -v /var/lib/containers/storage:/var/lib/containers/storage \
+  -v output:/output \
+  -v .tmp-config.toml:/config.toml \
+  quay.io/centos-bootc/bootc-image-builder:latest \
+  --type anaconda-iso \
+  --rootfs btrfs \
+  --local \
+  "$IMAGE"
+
+mv output/bootiso/install.iso ./fedora-silverblue.iso
+rm -rf output
